c0f627eba6cbd9e2a686b44dc29f31c1a59ef3e6
Thiago Neves Luz <84537567+RhoBlop@users.noreply.github.com>
2022-09-09 12:09:57 -0300

    Alteração de cores e inflação da Toolbar

--- app/src/main/java/thiago/neves/appGaleria/MainActivity.java
+++ app/src/main/java/thiago/neves/appGaleria/MainActivity.java
@@ -1,8 +1,13 @@
 package thiago.neves.appGaleria;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // setando toolbar para ser actionBar padrão
+        Toolbar toolbar = findViewById(R.id.tbMain);
+        setSupportActionBar(toolbar);
+    }
+
+    // preenche a toolbar com os items definidos nos arquivos em res/menu
+    @Override
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = this.getMenuInflater();
+        inflater.inflate(R.menu.main_activity_tb, menu);
+        return true;
+    }
+
+    // método chamado sempre que um item da Toolbar é selecionado
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // pega o id do item
+        switch (item.getItemId()) {
+            // caso seja o id do menu item da camêra, intent para abrir camêra é executado
+            case R.id.opCamera:
+                dispatchTakePictureIntent();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/thiago/neves/appGaleria/PhotoActivity.java
+++ app/src/main/java/thiago/neves/appGaleria/PhotoActivity.java
@@ -1,8 +1,14 @@
 package thiago.neves.appGaleria;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class PhotoActivity extends AppCompatActivity {
 
@@ -10,5 +16,36 @@ public class PhotoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
+
+        // define toolbar para ser actionBar padrão
+        Toolbar toolbar = findViewById(R.id.tbMain);
+        setSupportActionBar(toolbar);
+
+        // adiciona seta para voltar à MainActivity na toolbar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    // preenche a toolbar com os items definidos nos arquivos em res/menu
+    @Override
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.photo_activity_tb, menu);
+        return true;
+    }
+
+    // método chamado sempre que um item da Toolbar é selecionado
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // pega o id do item
+        switch (item.getItemId()) {
+            // caso seja o id do menu item de share, intent para compartilhar imagem é executado
+            case R.id.opShare:
+                sharePhoto();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
