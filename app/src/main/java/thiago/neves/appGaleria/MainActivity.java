675b6f6cd68b2e042b7a33063e34891b7905eae4
Thiago Neves Luz <84537567+RhoBlop@users.noreply.github.com>
2022-09-16 12:20:39 -0300

    Criação do Adapter da RecyclerView

--- app/src/main/java/thiago/neves/appGaleria/MainActivity.java
+++ app/src/main/java/thiago/neves/appGaleria/MainActivity.java
@@ -3,13 +3,22 @@ package thiago.neves.appGaleria;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    List<String> photos = new ArrayList<>();
+    MainAdapter mainAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,30 @@ public class MainActivity extends AppCompatActivity {
         // setando toolbar para ser actionBar padrão
         Toolbar toolbar = findViewById(R.id.tbMain);
         setSupportActionBar(toolbar);
+
+        // acessa o diretório Pictures
+        File dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        // acessa as imagens já salvas na pasta
+        File[] files = dir.listFiles();
+
+        // adiciona o caminho absoluto da imagem à arrayList de photos
+        for(int i = 0; i < files.length; i++) {
+            photos.add(files[i].getAbsolutePath());
+        }
+
+        mainAdapter = new MainAdapter(MainActivity.this, photos);
+
+        RecyclerView rvGallery = findViewById(R.id.rvGallery);
+        rvGallery.setAdapter(mainAdapter);
+
+        // largura de cada coluna da grid
+        float w = getResources().getDimension(R.dimen.itemWidth);
+        // número de colunas da recyclerView Grid
+        int numberOfColumns = Utils.calculateNoOfColumns(MainActivity.this, w);
+        // layout manager de grid para a recyclerView
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(MainActivity.this, numberOfColumns);
+        rvGallery.setLayoutManager(gridLayoutManager);
+
     }
 
     // preenche a toolbar com os items definidos nos arquivos em res/menu
---
+++ app/src/main/java/thiago/neves/appGaleria/PhotoViewHolder.java
@@ -0,0 +1,12 @@
+package thiago.neves.appGaleria;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PhotoViewHolder extends RecyclerView.ViewHolder{
+    public PhotoViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
